04dd86067a66505da0542ad36f643710d6860235
Krish1120 <48213650+Krish1120@users.noreply.github.com>
2022-10-29 21:17:11 +0530

    aws build

--- src/Components/axios.js
+++ src/Components/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const fetcher = axios.create({
-  baseURL: "http://192.168.0.188:50020",
+  baseURL: "http://ec2-13-126-106-185.ap-south-1.compute.amazonaws.com:50020",
 });
 //http://192.168.0.188:50020
 //https://radhika-admin-backend.herokuapp.com
+// http://ec2-13-126-106-185.ap-south-1.compute.amazonaws.com:50020
 export default fetcher;
